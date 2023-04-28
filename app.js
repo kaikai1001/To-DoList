638414bdd2bff5d5366d59097ec23de1d9fc512f
kaiaki1001 <cccjjmm09@gmail.com>
2023-04-28 22:53:29 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
-const port = 3000
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+//process.env.MONGODB_URI
+mongoose.connect('mongodb+srv://alpha:camp@cluster0.rdlqvcs.mongodb.net/todo-list?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
 
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.listen(port , () => {
-  console.log(`App is running on http://localhost:${port}`)
+app.listen(3000, () => {
+  console.log(`App is running on http://localhost:3000`)
 })
+
