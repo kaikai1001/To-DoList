e5ea3652b99e7fbd9f1f4b553b7396160b4695e2
kaiaki1001 <cccjjmm09@gmail.com>
2023-04-29 01:34:45 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphdb = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Todo = require('./models/todo') // 載入 Todo model
 const app = express()
 
@@ -26,11 +27,24 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean()  // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index', { todos }))// 將資料傳給 index 樣板
     .catch(error => console.log(error)) // 錯誤處理
+})
+
+app.get('/todos/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name  // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name })  // 存入資料庫
+    .then(() => { res.redirect('./') }) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
