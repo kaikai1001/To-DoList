e2209ccc5c93e995e5e1a33f1e3b8f92e0e13348
kaiaki1001 <cccjjmm09@gmail.com>
2023-04-30 16:08:51 +0800

    refractor: modify mongoose config

--- app.js
+++ app.js
@@ -1,40 +1,24 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphdb = require('express-handlebars')
 const bodyParser = require('body-parser')
-const Todo = require('./models/todo') // 載入 Todo model
-const todo = require('./models/todo')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+require('./config/mongoose')
 const app = express()
 
 app.engine('hbs', exphdb({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config()
+// }
 
-//process.env.MONGODB_URI
-mongoose.connect('mongodb+srv://alpha:camp@cluster0.rdlqvcs.mongodb.net/todo-list?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
 
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
-
-
 
 
 app.listen(3000, () => {
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,19 +1,9 @@
-const mongoose = require('mongoose')
 const Todo = require('../todo') // 載入 todo model
-
-//process.env.MONGODB_URI
-mongoose.connect('mongodb+srv://alpha:camp@cluster0.rdlqvcs.mongodb.net/todo-list?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  console.log('mongodb connected!')
-  for(let i = 0; i < 10 ;i++) {
-    Todo.create({name:`name-${i}`})
+  for (let i = 0; i < 10; i++) {
+    Todo.create({ name: `name-${i}` })
   }
   console.log('done')
 })
